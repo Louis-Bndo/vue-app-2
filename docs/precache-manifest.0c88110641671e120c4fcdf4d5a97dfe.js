12a9525dd40f3d5799852fd3d55d8222c7ac44c3
Louis Bernardeau <lbernardeau@la-joliverie.com>
2021-02-09 15:09:01 +0100

    test 3 github pages

---
+++ docs/precache-manifest.0c88110641671e120c4fcdf4d5a97dfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f62267e5236c323270",
+    "url": "/vue-app-2/css/app.b504474a.css"
+  },
+  {
+    "revision": "c7bb7c2106d9faee190265229217be8c",
+    "url": "/vue-app-2/img/zakumiz.c7bb7c21.jpg"
+  },
+  {
+    "revision": "6f0648ac1142cdecd947d2dfe8cf54e3",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "548a45e51e091c52f03a",
+    "url": "/vue-app-2/js/about.948006b8.js"
+  },
+  {
+    "revision": "e9f62267e5236c323270",
+    "url": "/vue-app-2/js/app.38be2605.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
